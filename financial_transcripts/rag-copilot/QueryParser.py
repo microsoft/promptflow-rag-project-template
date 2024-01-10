b099a6f177e155094f01234c20dc1e2cfccc68b4
Journey McDowell <jomcdowe@microsoft.com>
2024-01-10 11:46:14 -0800

    Updating QueryParser to use chat history for last query

--- financial_transcripts/rag-copilot/QueryParser.py
+++ financial_transcripts/rag-copilot/QueryParser.py
@@ -1,15 +1,29 @@
 from promptflow import tool
 import re
+import pdb
 
 @tool
-def query_parser(query: str):
-    # Extract year
-    year = re.search(r'FY(\d{2})', query).group(1)
+def query_parser(query: str, conversation):
+    try:
+        # Extract year
+        year = re.search(r'FY(\d{2})', query).group(1)
 
-    # Extract quarter
-    quarter = re.search(r'Q(\d)', query).group(1)
+        # Extract quarter
+        quarter = re.search(r'Q(\d)', query).group(1)
 
-    return {
-      'Year': str(year),
-      'Quarter': quarter
-    }
+        return {
+            'Year': str(year),
+            'Quarter': quarter
+        }
+    except:
+        last_query = conversation[0]['inputs']['query']
+        # Extract year
+        year = re.search(r'FY(\d{2})', last_query).group(1)
+
+        # Extract quarter
+        quarter = re.search(r'Q(\d)', last_query).group(1)
+
+        return {
+            'Year': str(year),
+            'Quarter': quarter
+        }
