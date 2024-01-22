8f3f13aa85fe4873a9424d978526c6d417e73cd9
Journey McDowell <jomcdowe@microsoft.com>
2024-01-22 11:28:33 -0800

    Attempting persistent output for bypassing nodes

---
+++ financial_transcripts/rag-copilot/KeepContext.py
@@ -0,0 +1,13 @@
+from promptflow import tool
+
+
+# The inputs section will change based on the arguments of the tool function, after you save the code
+# Adding type to arguments and return value will help the system show the types properly
+# Please update the function name/signature per need
+@tool
+def keep_context(retrieved_docs: object) -> str:
+    if not retrieved_docs:
+        already_retrieved = False
+    else:
+        already_retrieved = True
+    return already_retrieved
--- financial_transcripts/rag-copilot/QueryParser.py
+++ financial_transcripts/rag-copilot/QueryParser.py
@@ -3,7 +3,8 @@ import re
 import pdb
 
 @tool
-def query_parser(query: str, conversation):
+def query_parser(query: str, conversation, already_retrieved):
+    '''
     try:
         # Extract year
         year = re.search(r'FY(\d{2})', query).group(1)
@@ -27,3 +28,14 @@ def query_parser(query: str, conversation):
             'Year': str(year),
             'Quarter': quarter
         }
+    '''
+    # Extract year
+    year = re.search(r'FY(\d{2})', query).group(1)
+
+    # Extract quarter
+    quarter = re.search(r'Q(\d)', query).group(1)
+
+    return {
+        'Year': str(year),
+        'Quarter': quarter
+    }
