6435db38310ae9f42c202e6f0b7bf19e258950b8
Hossein Khadivi Heris <hokhadiv@microsoft.com>
2024-02-01 14:53:29 -0800

    add postgres connection

---
+++ financial_transcripts/connections/connect_postgres.py
@@ -0,0 +1,45 @@
+from promptflow import PFClient
+from promptflow.entities import CustomConnection
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+from dotenv import dotenv_values
+
+config = dotenv_values("../../.env")
+
+# Get a pf client to manage connections
+pf = PFClient()
+
+if config["KEYS_FROM"] == "KEYVAULT":
+    print("keyvault was selected.")
+    keyVaultName = config["KEY_VAULT_NAME"]
+    KVUri = f"https://{keyVaultName}.vault.azure.net"
+
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=KVUri, credential=credential)
+
+    # Initialize a custom connection object
+    connection = CustomConnection(
+        name="postgres_connection",
+        # Secrets is a required field for custom connection
+        secrets={"my_key": "DUMMY"},  # unsupported
+        configs={
+            "conn_string": client.get_secret(
+                "COSMOSDB-POSTGRES-CONN-STRING"
+            ).value,
+        },
+    )
+else:
+    print(".env was selected.")
+    # Initialize an AzureOpenAIConnection object
+    connection = CustomConnection(
+        name="postgres_connection",
+        # Secrets is a required field for custom connection
+        secrets={"my_key": "DUMMY"},  # unsupported
+        configs={
+            "conn_string": config["COSMOSDB_POSTGRES_CONN_STRING"],
+        },
+    )
+
+# Create the connection, note that all secret values will be scrubbed in the returned result
+result = pf.connections.create_or_update(connection)
+print(result)
