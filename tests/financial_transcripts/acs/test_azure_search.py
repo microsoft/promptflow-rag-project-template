ec8af2803aa69181474150df268917f1fec98430
HosseinHeris <hokhadiv@microsoft.com>
2024-04-18 12:46:02 -0700

    add test for main test flow

---
+++ tests/financial_transcripts/acs/test_azure_search.py
@@ -0,0 +1,17 @@
+import pytest
+
+def test_rag_azuresearch_flow_runs():
+    '''
+    The goal of the test: flow runs without error
+    Out of scope: accuracy/relevance of output 
+    '''
+    import promptflow
+    pf = promptflow.PFClient()
+
+    output = pf.flows.test(
+    "financial_transcripts/rag-azure-search/flow.dag.yaml",
+        inputs={
+            "chat_history": [],
+            "query": "What is the growth rate of Azure ML revenue in FY23Q1?",
+        },
+    )
